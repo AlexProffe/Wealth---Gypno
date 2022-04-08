ce0238cc454076ed4b77ea8a9c20f852d2c03719
Alex <aliaksei.sholakhau@netcracker.com>
2022-04-08 19:14:17 +0300

    Add Modal for Contact
    Add Modal for Contact

--- js/main.js
+++ js/main.js
@@ -17,7 +17,54 @@ const burgerManager = () => {
 
     function toggleModalVisibility() {
         burgerModal.classList.toggle("hidden");
-    };
+    }
 };
 
 burgerManager();
+
+const modalManager = () => {
+    const modal = document.getElementById("modal");
+    const modalForm = document.querySelector(".modal-form");
+    const openModalButtons = document.querySelectorAll(
+        'button[data-action="modal"]'
+    );
+    const submitButton = modal.querySelector(".form-button");
+
+    openModalButtons.forEach((button) => {
+        button.addEventListener("click", () => {
+            toggleModalVisibility();
+        });
+    });
+
+    modalForm.addEventListener("click", (e) => {
+        e.stopPropagation();
+    });
+
+    modal.addEventListener("click", () => {
+        toggleModalVisibility();
+    });
+
+    modalForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const [name, phone, email, request] = modalForm;
+
+        const formData = {
+            name: name.value,
+            phone: phone.value,
+            email: email.value,
+            request: request.value,
+        };
+
+        console.log(formData);
+
+        if (modalForm.checkValidity()) {
+            toggleModalVisibility();
+        }
+    });
+
+    function toggleModalVisibility() {
+        modal.classList.toggle("hidden");
+    }
+};
+
+modalManager();
